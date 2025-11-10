ab7e65b51bedc10f9358bd7247b95c14ec24c1df
SatishRU <ujjammanavar.satish@gmail.com>
2025-11-10 18:47:00 +0530

    Added FloatingActionButton UI components

--- app/src/main/java/com/example/jc_ui_components/MainActivity.kt
+++ app/src/main/java/com/example/jc_ui_components/MainActivity.kt
@@ -16,17 +16,21 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.selection.selectable
 import androidx.compose.foundation.selection.selectableGroup
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.outlined.Info
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Checkbox
 import androidx.compose.material3.CheckboxDefaults
+import androidx.compose.material3.ExtendedFloatingActionButton
+import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.RadioButton
@@ -45,6 +49,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
@@ -87,6 +92,7 @@ fun HomeScreen() {
         JCSwitch()
         JCRadioButton()
         JCCheckBox()
+        JCFloatingButton()
     }
 }
 
@@ -256,7 +262,6 @@ fun JCRadioButton() {
 
     Row(
         modifier = Modifier
-            .selectableGroup()
             .fillMaxWidth()
             .padding(0.dp, 8.dp, 0.dp, 0.dp),
         horizontalArrangement = Arrangement.Center,
@@ -264,13 +269,6 @@ fun JCRadioButton() {
     ) {
         options.forEach { rbText ->
             Row(
-                modifier = Modifier.selectable(
-                    selected = (rbText == selectedOption),
-                    onClick = {
-                        selectedOption = rbText
-                        onOptionSelected(rbText)
-                    }
-                ),
                 verticalAlignment = Alignment.CenterVertically,
             ) {
                 RadioButton(
@@ -315,5 +313,56 @@ fun JCCheckBox() {
             )
         )
         Text(text = "CheckBox Text")
+    }
+}
+
+@Composable
+fun JCFloatingButton() {
+    val context = LocalContext.current
+
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(0.dp, 8.dp, 0.dp, 0.dp),
+        horizontalArrangement = Arrangement.Center,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        FloatingActionButton(
+            modifier = Modifier.height(30.dp),
+            onClick = {
+                Toast.makeText(context, "Floating Button  1 Clicked", Toast.LENGTH_SHORT).show()
+            },
+            containerColor = Green,
+            contentColor = Color.White
+        ) {
+            Text(text = "FAB 1")
+        }
+
+        Spacer(modifier = Modifier.width(16.dp))
+
+        FloatingActionButton(
+            modifier = Modifier.height(30.dp),
+            onClick = {
+                Toast.makeText(context, "Floating Button  2 Clicked", Toast.LENGTH_SHORT).show()
+            },
+            containerColor = Green,
+            contentColor = Color.White,
+            shape = RectangleShape
+        ) {
+            Icon(Icons.Filled.Add, "Add")
+        }
+
+        Spacer(modifier = Modifier.width(16.dp))
+
+        ExtendedFloatingActionButton(
+            modifier = Modifier.height(30.dp),
+            onClick = {
+                Toast.makeText(context, "Floating Button  3 Clicked", Toast.LENGTH_SHORT).show()
+            },
+            containerColor = Green,
+            contentColor = Color.White,
+            icon = { Icon(Icons.Filled.Add, "Add") },
+            text ={  Text(text = "FAB 3") }
+        )
     }
 }
