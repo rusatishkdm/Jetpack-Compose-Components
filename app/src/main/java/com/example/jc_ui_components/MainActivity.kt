4f62be7a3df4ee662b787b6bb18943ca709352d1
SatishRU <ujjammanavar.satish@gmail.com>
2025-11-11 11:38:37 +0530

    Added Material Buttons UI components

--- app/src/main/java/com/example/jc_ui_components/MainActivity.kt
+++ app/src/main/java/com/example/jc_ui_components/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -17,10 +18,13 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.safeContentPadding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.LocationOn
@@ -50,6 +54,8 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.platform.LocalContext
@@ -88,7 +94,9 @@ fun HomeScreen() {
     Column(
         modifier = Modifier
             .fillMaxSize()
-            .padding(16.dp),
+            .padding(16.dp)
+            .verticalScroll(rememberScrollState())
+            .safeContentPadding()
     ) {
         SayHelloText("Satish")
         JCButton("Click Me")
@@ -454,5 +462,23 @@ fun AllButtons() {
         ) {
             Text(text = "Button Custom Color", color = MaterialTheme.colorScheme.onPrimary)
         }
+    }
+
+    Row {
+        //Custom Gradient
+        val gradient = Brush.horizontalGradient(
+            colors = listOf(MaterialTheme.colorScheme.primary, MaterialTheme.colorScheme.secondary),
+            0f, 250F
+        )
+
+        Text(
+            text = "Custom Gradient",
+            color = Color.White,
+            modifier = Modifier
+                .clickable(onClick = {})
+                .clip(RoundedCornerShape(4.dp))
+                .background(brush = gradient)
+                .padding(12.dp)
+        )
     }
 }
